3a8e094b4ba358fa8bb11125c5e4c6736b36186d
LeatherBastard <kostrykinmark@yandex.ru>
2023-12-01 11:04:14 +0300

    fix: Add UserRepositoryTest class

---
+++ src/test/java/ru/practicum/shareit/user/UserRepositoryTest.java
@@ -0,0 +1,9 @@
+package ru.practicum.shareit.user;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@DataJpaTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class UserRepositoryTest {
+}
