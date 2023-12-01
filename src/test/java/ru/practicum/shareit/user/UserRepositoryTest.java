d06804cd252de2c9062c3e69ae1b9a252ee52e45
LeatherBastard <kostrykinmark@yandex.ru>
2023-12-01 11:36:09 +0300

    fix: Add UserRepository tests

--- src/test/java/ru/practicum/shareit/user/UserRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/user/UserRepositoryTest.java
@@ -1,9 +1,28 @@
 package ru.practicum.shareit.user;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.DirtiesContext;
+import ru.practicum.shareit.exception.EntityNotFoundException;
+import ru.practicum.shareit.user.repository.UserRepository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void findById() {
+        assertThrows(EntityNotFoundException.class, () -> userRepository.findById(1));
+    }
+
+    @Test
+    void findAll() {
+        assertEquals(0, userRepository.findAll().size());
+    }
 }
